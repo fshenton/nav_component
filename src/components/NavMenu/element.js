443fa4451d5f93e452b24b3341d779f913e4f913
fshenton <fshenton@gmail.com>
2019-08-13 13:21:08 +0100

    Basic template set up. NavMenu is creating a shadow dom and appending template HTML to it.

--- src/components/NavMenu/element.js
+++ src/components/NavMenu/element.js
@@ -4,6 +4,13 @@ class NavMenu extends HTMLElement {
 	constructor(){
 		super();
 		console.dir(template);
+
+		const shadow = this.attachShadow({"mode": "open"});
+		const clone = document.importNode(template.content, true);
+
+
+
+		shadow.appendChild(clone);
 	}
 }
 
--- src/components/NavMenu/template.js
+++ src/components/NavMenu/template.js
@@ -1,4 +1,13 @@
-const template = "heyooooo";
+const template = document.createElement("template");
 
+template.innerHTML = `
+	<nav>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
+	</nav>
+`;
 
 export default template;
